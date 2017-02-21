adb08c78156f026c60358fc2afd7883f42c6996c
Benedict Low <low_ben@rocketmail.com>
2017-02-21 23:53:35 +0800

    Attempt to change gitignore

---
+++ PSS Simulator/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//used to manage the highscores and maybe spawning of enemies in the future
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
