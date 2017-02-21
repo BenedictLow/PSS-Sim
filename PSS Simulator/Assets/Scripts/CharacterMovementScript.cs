7c59d0c4da72c7508cd696f16aea7877dd05bb65
Benedict Low <low_ben@rocketmail.com>
2017-02-21 23:41:45 +0800

    Create folders for organisational purposes

---
+++ PSS Simulator/Assets/Scripts/CharacterMovementScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello my friend");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
